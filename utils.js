f215086f2d4e812fe36d1d5d00e9c4c5880667ad
Coachflux <110670400+Coachflux@users.noreply.github.com>
2026-09-08 23:11:55 +0100

    Fix iframe sandbox attribute setting

--- utils.js
+++ utils.js
@@ -495,7 +495,7 @@ const PlayerManager = {
 
         if (iframe) {
             // CRITICAL: Sandbox attributes block redirects/popups while allowing video
-            iframe.setAttribute('sandbox', 'allow-scripts allow-same-origin allow-presentation allow-autoplay');
+            iframe.setAttribute('allow-scripts allow-same-origin allow-presentation allow-autoplay');
             iframe.setAttribute('referrerpolicy', 'no-referrer');
             iframe.setAttribute('loading', 'eager');
             iframe.src = src;
